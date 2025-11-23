73938835f8b95e472a2c3129c4026408a0c48701
Rohithashwa <rohangangodi@gmail.com>
2025-11-23 22:35:03 +0530

    Initial commit

---
+++ src/test/java/com/app/ecommerce_admin/EcommerceAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ecommerce_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
