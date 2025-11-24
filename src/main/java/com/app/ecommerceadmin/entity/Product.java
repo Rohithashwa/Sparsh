57be428b0957a791eae48dbd2b8d1cd38ee3648f
sunil <sunil.2701k@gmail.com>
2025-11-24 17:12:14 +0530

    adding datasource

--- src/main/java/com/app/ecommerceadmin/entity/Product.java
+++ src/main/java/com/app/ecommerceadmin/entity/Product.java
@@ -15,7 +15,7 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private UUID id;
+    private Long id;
 
     @Column(nullable = false)
     private String name;
